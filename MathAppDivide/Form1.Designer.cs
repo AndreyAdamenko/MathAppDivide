fab62e4b17309730d4598615062bf30b026c28df
AndreyHome <temafeyo@gmail.com>
2024-03-08 14:03:46 +0500

    Add project files.

---
+++ MathAppDivide/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace MathAppDivide
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 70);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(287, 23);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(332, 70);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(171, 23);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBox3.Location = new System.Drawing.Point(12, 99);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox3.Size = new System.Drawing.Size(491, 406);
+            this.textBox3.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(509, 69);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Далее";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(509, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Баллов";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(157, 47);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Уровень";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(509, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(22, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(509, 164);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(120, 25);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Правельных";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(509, 189);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(22, 25);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(509, 229);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(83, 25);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Ошибок";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(509, 254);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(22, 25);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "0";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label8.Location = new System.Drawing.Point(509, 297);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(88, 25);
+            this.label8.TabIndex = 4;
+            this.label8.Text = "Процент";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label9.Location = new System.Drawing.Point(509, 322);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(22, 25);
+            this.label9.TabIndex = 4;
+            this.label9.Text = "0";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label10.Location = new System.Drawing.Point(305, 69);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(21, 21);
+            this.label10.TabIndex = 6;
+            this.label10.Text = "=";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(639, 517);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Деление";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Button button1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+    }
+}
---
+++ MathAppDivide/Form1.cs
@@ -0,0 +1,257 @@
+using System.Media;
+
+namespace MathAppDivide
+{
+    public partial class Form1 : Form
+    {
+        double dividend = 0;
+        double divisor = 0;
+
+        int points = 0;
+        int miss = 0;
+        int sucsess = 0;
+
+        int level = 1;
+
+        int rounds = 0;
+
+        class CheckResult
+        {
+            public int value { get; set; }
+            public bool isCorrect { get; set; }
+            public bool isSucces { get; set; }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            newSolution();
+
+            UpdateLabels();
+
+            button1.Click += (x, y) =>
+            {
+                var result = checkResult();
+
+                UpdateLabels();
+
+                if (!result.isCorrect) return;
+
+                textBox2.Text = "";
+
+
+                if (result.isSucces)
+                    textBox3.Text = $"{dividend}/{divisor}={result.value}\r\n" + textBox3.Text;
+                else
+                    textBox3.Text = $"{dividend}/{divisor}={result.value} (Не правильно)\r\n" + textBox3.Text;
+
+                newSolution();
+            };
+
+            FormClosing += Form1_FormClosing;
+        }
+
+        private void Form1_FormClosing(object? sender, FormClosingEventArgs e)
+        {
+            var r = MessageBox.Show("Внимание", "Закрыть программу?", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+            if (r != DialogResult.Yes)
+                e.Cancel = true;
+        }
+
+        CheckResult checkResult()
+        {
+            int result;
+
+            if (int.TryParse(textBox2.Text.Trim(), out result))
+            {
+                var isSucces = false;
+
+                if (dividend / divisor == result)
+                {
+                    points += 10 * level;
+                    sucsess++;
+
+                    Play(true);
+
+                    isSucces = true;
+                }
+                else
+                {
+                    points -= level;
+                    miss++;
+
+                    Play(false);
+                }
+
+                rounds++;
+
+                levelUpdate();
+
+                var perc = Math.Round((double)sucsess / (sucsess + miss), 2) * 100;
+
+                label9.Text = perc.ToString();
+
+                return new CheckResult { value = result, isCorrect = true, isSucces = isSucces };
+            }
+            else
+            {
+                MessageBox.Show("Не могу понять ответ");
+
+                return new CheckResult { value = -1, isCorrect = false, isSucces = false };
+            }
+        }
+
+        void newSolution()
+        {
+            var t = GenerateDivisibleNumbers(2 + level, 2);
+
+            dividend = t.Item1;
+            divisor = t.Item2;
+
+            textBox1.Text = $"{dividend}/{divisor}";
+
+            //textBox2.Text = (dividend / divisor).ToString();
+        }
+
+        static Tuple<double, double> GenerateDivisibleNumbers3(int numberOfDigitsDivisor, int numberOfDigitsMultip)
+        {
+            Random random = new Random();
+
+            double divisor = 0;
+            double multip = 0;
+            double dividend = 0;
+
+            while (!isCorrect())
+            {
+                divisor = Math.Round(random.NextDouble() * Math.Pow(10, numberOfDigitsDivisor));
+
+                multip = Math.Round(random.NextDouble() * Math.Pow(10, numberOfDigitsMultip));
+
+                dividend = divisor * multip;
+            }
+
+            return Tuple.Create(dividend, divisor);
+
+            bool isCorrect()
+            {
+                if (dividend == 0 || divisor == 0) return false;
+                if (dividend == divisor) return false;
+                if (divisor == 1) return false;
+                //if (dividend.ToString().Length != numberOfDigitsDividend) return false;
+                if (divisor.ToString().Length != numberOfDigitsDivisor) return false;
+
+                return true;
+            }
+        }
+
+        static Tuple<int, int> GenerateDivisibleNumbers(int numberOfDigitsDividend, int numberOfDigitsDivisor)
+        {
+            Random random = new Random();
+
+            int dividend = 0;
+            int divisor = 0;
+
+            while (!isCorrect())
+            {
+                // Генерация делимого
+                int minDividend = (int)Math.Pow(10, numberOfDigitsDividend - 1);
+                int maxDividend = (int)Math.Pow(10, numberOfDigitsDividend) - 1;
+                dividend = random.Next(minDividend, maxDividend + 1);
+
+                // Генерация делителя
+                int minDivisor = (int)Math.Pow(10, numberOfDigitsDivisor - 1);
+                int maxDivisor = (int)Math.Pow(10, numberOfDigitsDivisor) - 1;
+                divisor = random.Next(minDivisor, maxDivisor + 1);
+
+                // Убеждаемся, что делимое делится на делитель без остатка
+                int quotient = dividend / divisor;
+                dividend = quotient * divisor;
+            }
+
+            return Tuple.Create(dividend, divisor);
+
+            bool isCorrect()
+            {
+                if (dividend == 0 || divisor == 0) return false;
+                if (dividend == divisor) return false;
+                if (divisor == 1) return false;
+                if (dividend.ToString().Length != numberOfDigitsDividend) return false;
+                if (divisor.ToString().Length != numberOfDigitsDivisor) return false;
+
+                return true;
+            }
+        }
+
+        static Tuple<double, double> GenerateDivisibleNumbers2(int numberOfDigitsDividend, int numberOfDigitsDivisor)
+        {
+            Random random = new Random();
+
+            double dividend = 0;
+            double divisor = 0;
+
+            while (!isCorrect())
+            {
+                // Генерация делимого
+                //double minDividend = (double)Math.Pow(10, numberOfDigitsDividend - 1);
+                //double maxDividend = (double)Math.Pow(10, numberOfDigitsDividend) - 1;
+                dividend = Math.Round(random.NextDouble() * Math.Pow(10, numberOfDigitsDividend));
+
+                // Генерация делителя
+                //double minDivisor = (double)Math.Pow(10, numberOfDigitsDivisor - 1);
+                //double maxDivisor = (double)Math.Pow(10, numberOfDigitsDivisor) - 1;
+                divisor = Math.Round(random.NextDouble() * Math.Pow(10, numberOfDigitsDivisor));
+
+                // Убеждаемся, что делимое делится на делитель без остатка
+                double quotient = dividend / divisor;
+                dividend = quotient * divisor;
+            }
+
+
+            return Tuple.Create(dividend, divisor);
+
+            bool isCorrect()
+            {
+                if (dividend == 0 || divisor == 0) return false;
+                if (dividend == divisor) return false;
+                if (divisor == 1) return false;
+                if (dividend.ToString().Length != numberOfDigitsDividend) return false;
+                if (divisor.ToString().Length != numberOfDigitsDivisor) return false;
+
+                return true;
+            }
+        }
+
+        void levelUpdate()
+        {
+            level = (sucsess / 10) + 1;
+
+            if (level < 1) level = 1;
+
+            if (level > 7) level = 7;
+        }
+
+        void UpdateLabels()
+        {
+            label3.Text = points.ToString();
+            label5.Text = sucsess.ToString();
+            label7.Text = miss.ToString();
+            label2.Text = $"Уровень {level} по {10 * level} баллов";
+        }
+
+        void Play(bool succes)
+        {
+            Task.Run(() =>
+            {
+                var sound = succes ? @".\sound\success-1-6297.wav" : @".\sound\error-call-to-attention-129258.wav";
+
+                SoundPlayer simpleSound = new SoundPlayer(sound);
+                simpleSound.Play();
+            });
+        }
+    }
+}
